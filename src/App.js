73231e2f53c1e851ec2b34f596662113e91befba
Mridula Chowdhary <mridulachowdhary21@gmail.com>
2024-07-06 09:50:03 +0530

    Implemented Lazy loading

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
+import { Suspense,lazy } from 'react';
 import { BrowserRouter,Route, Routes } from 'react-router-dom';
 import './App.css';
-import Home from './pages/Home';
-import Cart from './components/Cart';
-import Menus from './components/Menus';
-import Success from './pages/Success';
-
+const Home = lazy(() => import('./pages/Home'));
+const Cart = lazy(() => import('./components/Cart'));
+const Menus = lazy(() => import('./components/Menus'));
+const Success = lazy(() => import('./pages/Success'));
 
 function App() {
+
   return (
     <>
     <div className="App">
     <BrowserRouter>
+    <Suspense fallback ={<div>loading...</div>}>
     <Routes>
       <Route path='/' element={<Home/>} />
       <Route path='/cart' element={<Cart/>}/>
       <Route path= '/menus' element={<Menus/>}/>
-       <Route path='/success' element={<Success/>}/>
-     
+       <Route path='/success' element={<Success/>}/>  
     </Routes>
+    </Suspense>
     </BrowserRouter>
     </div>
     </>
